395a93dfdcdb8f30e892d5a494f5fc777e016bca
Dimidrol <dkonygin@gmail.com>
2015-02-05 20:04:52 +0300

    add class

---
+++ DKActivityIndicatorView-Swift.swift
@@ -0,0 +1,24 @@
+//
+//  DKActivityIndicatorView-Swift.swift
+//  curling
+//
+//  Created by Dimidrol on 05.02.15.
+//  Copyright (c) 2015 Dmitry Konygin. All rights reserved.
+//
+
+import UIKit
+
+class DKActivityIndicatorView_Swift: UIView {
+
+    
+    
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
